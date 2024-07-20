f85f6837aff2102126501a728c4be985904cbe60
Danish Zar Taj <159439065+danishzartaj@users.noreply.github.com>
2024-07-20 22:43:25 +0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-reverses = "python is fun"
-words = reverses.split()
-Empty = ""
-index = len(words) - 1
-while index >= 0:
-    Empty += words[index] + " " 
-    index -= 1
-print(Empty.strip()) # Output become fun is python   
-
